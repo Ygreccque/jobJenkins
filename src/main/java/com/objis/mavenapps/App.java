4e8cedfb24d9ed217262d8d6381622183aa60927
YGreccque M <mlx.teta@yahoo.fr>
2016-10-27 16:08:22 +0200

    Commit

--- src/main/java/com/objis/mavenapps/App.java
+++ src/main/java/com/objis/mavenapps/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Ici c'est la terre qui vous passe le bonjour" );
+        System.out.println( "Hello World! ****** Ici c'est la terre qui vous passe le bonjour" );
     }
 }
