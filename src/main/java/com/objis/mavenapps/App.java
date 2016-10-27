6558deb286a81f903c5a41b8e5fe5ef436142709
YGreccque M <mlx.teta@yahoo.fr>
2016-10-27 16:15:02 +0200

    ****d

--- src/main/java/com/objis/mavenapps/App.java
+++ src/main/java/com/objis/mavenapps/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! ****** Ici c'est la terre qui vous passe le bonjour" );
+		System.out.println( "Hello World! ****** Ici c'est la terre qui vous passe le bonjour" );
     }
 }
